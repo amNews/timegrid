b89a10d3c4e0053768ea2ae8be3ca34de90416f4
Anton Mueller <anton.mueller@eccenca.com>
2017-03-26 01:39:06 +0100

    duobbled some expressions

---
+++ resources/lang/de_DE/app.php
@@ -0,0 +1,102 @@
+<?php
+
+return  [
+  'saas' => [
+    'plan_limit_reached' => 'You reached your plan limit',
+    'plan_limit_reached' => 'You reached your plan limit',
+  ],
+  'btn' => [
+    'get_to_dashboard' => 'Get to Dashboard',
+    'get_to_dashboard' => 'Get to Dashboard',
+    'update'           => 'Update',
+    'update'           => 'Update',
+  ],
+  'business' => [
+    'category' => [
+      'garage'       => 'Car Mechanic',
+      'doctor'       => 'Healthcare',
+      'photography'  => 'Photography',
+      'spa'          => 'Spa & Beauty',
+      'martial-arts' => 'Martial Arts',
+      'yoga'         => 'Yoga',
+      'consulting'   => 'Consulting',
+      'hairdresser'  => 'Hairdresser',
+      'beauty'       => 'Beauty & Healthcare',
+      
+      'garage'       => 'Car Mechanic',
+      'doctor'       => 'Healthcare',
+      'photography'  => 'Photography',
+      'spa'          => 'Spa & Beauty',
+      'martial-arts' => 'Martial Arts',
+      'yoga'         => 'Yoga',
+      'consulting'   => 'Consulting',
+      'hairdresser'  => 'Hairdresser',
+      'beauty'       => 'Beauty & Healthcare',
+    ],
+  ],
+  'dateformat' => [
+    'carbon'         => 'm/d/Y',
+    'datetimepicker' => 'MM/DD/YYYY',
+  ],
+  'footer' => [
+    'demo'  => 'Remember this is a DEMO version.',
+    'demo'  => 'Remember this is a DEMO version.',
+    'local' => 'Local environment advice.',
+    'local' => 'Local environment advice.',
+  ],
+  'gender' => [
+    'F' => 'Female',
+    'F' => 'Female',
+    'M' => 'Male',
+    'M' => 'Male',
+  ],
+  'label' => [
+    'attention'       => 'Attention',
+    'attention'       => 'Attention',
+    'select_timezone' => 'TimeZone',
+    'select_timezone' => 'TimeZone',
+  ],
+  'msg' => [
+    'invalid_token'     => 'We found some validation problems, please submit your form again.',
+    'invalid_token'     => 'We found some validation problems, please submit your form again.',
+    'general_exception' => 'Sorry, there was a problem. Don\'t worry, we are working on it ;)',
+    'invalid_url'       => 'The link you try to access does not exist, try browsing the directory.',
+    'slug_is_available' => 'The :baseurl name is available. Register it now.',
+    'general_exception' => 'Sorry, there was a problem. Don\'t worry, we are working on it ;)',
+    'invalid_url'       => 'The link you try to access does not exist, try browsing the directory.',
+    'slug_is_available' => 'The :baseurl name is available. Register it now.',
+  ],
+  'name' => 'timegrid.io',
+  'nav'  => [
+    'help'    => 'Help',
+    'login'   => 'Login',
+    'logout'  => 'Logout',
+    'manager' => [
+      'business' => [
+        'menu'     => 'My Businesses',
+        'register' => 'SignUp Business',
+      ],
+    ],
+    'manual'      => 'User Manual',
+    'preferences' => 'Preferences',
+    'register'    => 'Register',
+    'user'        => [
+      'business' => [
+        'menu'             => 'Directory',
+        'my_appointments'  => 'My Reservations',
+        'my_subscriptions' => 'My Subscriptions',
+        'selector'         => 'Browse',
+      ],
+    ],
+    'wizard' => 'Help',
+  ],
+  'notifications' => [
+    'title' => 'Recently',
+  ],
+  'search' => [
+    'msg' => [
+      'no_results' => 'We found nothing with this criteria: :criteria',
+    ],
+    'placeholder' => 'search',
+  ],
+];
